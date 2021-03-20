1c0dba40afe8b8ed66377527e89f2e584ece7355
jbabbo2 <jbabbo2@illinois.edu>
2021-03-20 22:21:34 +0000

    Update keyboard.c

--- student-distrib/keyboard.c
+++ student-distrib/keyboard.c
@@ -1,1 +1,20 @@
 #include "keyboard.h"
+#include "lib.h"
+
+char = scan
+//0's in order stand in for nothing, escape, enter, left shift
+char scan_code_1[128] = {
+    0, 0, '1', '2', '3', '4', '5', '6', '7', '8',
+    '9', '0', '-', '=', '\b', '\t', 'q', 'w', 'e', 'r',
+    't', 'y', 'u', 'i', 'o', 'p', '[', ']', 0, 'a'
+    's', 'd', 'f', 'g', 'h', 'j', 'k', 'l', ';', '\'',
+    '`', 0, '\\', 'z', 'x', 'c', 'v', 'b', 'n' 'm',
+    }
+
+void keyboard_init(){
+    enable_irq(KB_IRQ)
+}
+
+void keyboard_handler(){
+    byte = inb(KB_PORT);
+}
