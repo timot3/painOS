52298ac1d2a6d46ebc88a86c04e6db13e70cfac6
Hassan F <1hassanfarooq@gmail.com>
2021-03-20 15:52:47 -0500

    Starting pic stuff

--- student-distrib/i8259.c
+++ student-distrib/i8259.c
@@ -11,6 +11,23 @@ uint8_t slave_mask;  /* IRQs 8-15 */
 
 /* Initialize the 8259 PIC */
 void i8259_init(void) {
+    // Using https://wiki.osdev.org/8259_PIC as reference
+
+    // Start initialization sequence
+    outb(ICW1, MASTER_8259_PORT);
+    outb(ICW1, SLAVE_8259_PORT);
+
+    // Send high bits of vector number
+    outb(ICW2_MASTER, MASTER_8259_PORT);
+    outb(ICW2_SLAVE, SLAVE_8259_PORT);
+
+    // Tell slave master exists, give slave address
+    outb(ICW3_MASTER, MASTER_8259_PORT);
+    outb(ICW3_SLAVE, SLAVE_8259_PORT);
+
+    // Send normal/auto EOI
+    outb(ICW4, MASTER_8259_PORT);
+    outb(ICW4, SLAVE_8259_PORT);
 }
 
 /* Enable (unmask) the specified IRQ */
