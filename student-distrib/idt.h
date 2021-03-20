2e8c6613b26e1e26fa0c0388b4d666a37245cb29
jbabbo2 <jbabbo2@illinois.edu>
2021-03-20 21:23:38 +0000

    Update idt.h

--- student-distrib/idt.h
+++ student-distrib/idt.h
@@ -3,6 +3,28 @@
 
 void idt_error();
 
+void DIV_BY_ZERO();
+void RESERVED_INT();
+void NMI_INTERRUPT();
+void BREAKPOINT();
+void OVERFLOW();
+void BOUND_RANGE_EXCEEDED();
+void INVALID_OPCODE();
+void DEVICE_NOT_AVAILABLE();
+void DOUBLE_FAULT();
+void COPROCESSOR_SEGMENT_OVERRUN();
+void INVALID_TSS();
+void SEGMENT_NOT_PRESENT();
+void STACK_SEGMENT_FAULT();
+void GENERAL_PROTECTION();
+void PAGE_FAULT();
+void INTEL_RESERVED();
+void MATH_FAULT();
+void ALIGNMENT_CHECK();
+void MACHINE_CKECK();
+void SIMD_FLOATING_POINT_EXCEPTION();
+void initialize_idt();
+
 void initialize_idt();
 
 #define NUM_SYSTEM_INTERRUPTS 32
