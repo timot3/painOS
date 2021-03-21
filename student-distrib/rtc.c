c3bdac8cbba4ca003e9ddda1bb855ecf9d41eb6f
Hassan F <1hassanfarooq@gmail.com>
2021-03-21 13:07:01 -0500

    Better comments

--- student-distrib/rtc.c
+++ student-distrib/rtc.c
@@ -3,10 +3,11 @@
 
 void initialize_rtc() {
     // Using https://wiki.osdev.org/RTC as reference
+
     cli();
-    outb(RTC_B, RTC_PORT);
-    char prev = inb(CMOS_PORT);
-    outb(RTC_B, RTC_PORT);
-    outb(prev | 0x40, CMOS_PORT);
+    outb(RTC_B, RTC_PORT);        // Disable NMI
+    char prev = inb(CMOS_PORT);   // Read current value
+    outb(RTC_B, RTC_PORT);        // Set index again (read resets index to register D)
+    outb(prev | 0x40, CMOS_PORT); // Turn off bit 6 of register B
     enable_irq(RTC_IRQ);
 }
--- student-distrib/rtc.h
+++ student-distrib/rtc.h
@@ -1,13 +1,14 @@
 #ifndef RTC_H
 #define RTC_H
 
+// From https://wiki.osdev.org/RTC
 #define RTC_PORT 0x70
 #define CMOS_PORT 0x71
-
 #define RTC_A 0x8A
 #define RTC_B 0x8B
 #define RTC_C 0x8C
 
+// Given in lectures/notes
 #define RTC_IRQ 0x8
 
 void initialize_rtc();
