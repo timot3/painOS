59787faf4d1115c168e3b3459a2cc8d21d571df6
jbabbo2 <jbabbo2@illinois.edu>
2021-03-21 02:30:06 +0000

    Update keyboard.c

--- student-distrib/keyboard.c
+++ student-distrib/keyboard.c
@@ -1,20 +1,24 @@
 #include "keyboard.h"
 #include "lib.h"
+#include "i8259.h"
 
-char = scan
-//0's in order stand in for nothing, escape, enter, left shift
-char scan_code_1[128] = {
+//0's in order stand in for nothing, escape, enter, left shift, right shift, left alt
+char scan_code_1[256] = {
     0, 0, '1', '2', '3', '4', '5', '6', '7', '8',
     '9', '0', '-', '=', '\b', '\t', 'q', 'w', 'e', 'r',
-    't', 'y', 'u', 'i', 'o', 'p', '[', ']', 0, 'a'
+    't', 'y', 'u', 'i', 'o', 'p', '[', ']', 0, 'a',
     's', 'd', 'f', 'g', 'h', 'j', 'k', 'l', ';', '\'',
-    '`', 0, '\\', 'z', 'x', 'c', 'v', 'b', 'n' 'm',
-    }
+    '`', 0, '\\', 'z', 'x', 'c', 'v', 'b', 'n', 'm',
+    ',', '.', '/', 0, '*', 0, ' '};
 
 void keyboard_init(){
-    enable_irq(KB_IRQ)
+    enable_irq(KB_IRQ);
 }
 
 void keyboard_handler(){
-    byte = inb(KB_PORT);
+    char byte = inb(KB_PORT);
+    char character = scan_code_1[byte];
+    printf(character);
+    send_eoi(KB_IRQ);
 }
+
