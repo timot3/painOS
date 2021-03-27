66ab457763b71d661f197d86eb6f33c1f7215619
jbabbo2 <jbabbo2@illinois.edu>
2021-03-27 04:32:00 -0500

    Revert "Update keyboard.h"
    This reverts commit e499283e4806a1b17e9e55a4903f97bdb3109405

--- student-distrib/keyboard.h
+++ student-distrib/keyboard.h
@@ -4,17 +4,7 @@
 #define KB_PORT 0x60
 #define KB_IRQ 1
 
-#define SHIFT_PRESS 200
-#define CAP_PRESS 201
-#define CTRL_PRESS 202
-#define SHIFT_RELEASE 203
-#define CTRL_RELEASE 204
-#define ALT_PRESS 205
-#define ALT_RELEASE 206
-#define ASCII_L 108
-
 void keyboard_init();
 void keyboard_handler();
-void keyboard_print(int byte);
 
 #endif /* KEYBOARD_H */
