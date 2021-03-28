08a5fe044c396fcb0252f1c9e3f27c3a9ab8e7f5
jbabbo2 <jbabbo2@illinois.edu>
2021-03-28 21:38:21 +0000

    Update terminal.h

--- student-distrib/terminal.h
+++ student-distrib/terminal.h
@@ -8,6 +8,7 @@ int terminal_open(const int* filename);
 int terminal_close(int fd);
 int terminal_read(int fd, unsigned char* buf, int nbytes);
 int terminal_write(int fd, unsigned char* buf, int nbytes);
+int terminal_buf_save(unsigned char* buf);
 
 unsigned char terminal_buf[128];
 
