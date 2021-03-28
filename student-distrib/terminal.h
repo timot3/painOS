d38718034a63249bf0aca86f31afb02bd55d9e36
jbabbo2 <jbabbo2@illinois.edu>
2021-03-28 08:50:49 +0000

    Update terminal.h

--- student-distrib/terminal.h
+++ student-distrib/terminal.h
@@ -2,9 +2,13 @@
 #define TERMINAL_H
 
 #define VGA_WIDTH 80
+#define TERM_BUF_SIZE 128
 
 int terminal_open(const int* filename);
 int terminal_close(int fd);
 int terminal_read(int fd, unsigned char* buf, int nbytes);
 int terminal_write(int fd, unsigned char* buf, int nbytes);
+
+volatile int term_buf_location;
+
 #endif /* TERMINAL_H */
