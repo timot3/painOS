39c7d603ff40025a77b3b263bcde28956374361b
jbabbo2 <jbabbo2@illinois.edu>
2021-03-28 19:44:05 +0000

    Update terminal.c

--- student-distrib/terminal.c
+++ student-distrib/terminal.c
@@ -1,34 +1,52 @@
 #include "terminal.h"
 #include "lib.h"
+#include "keyboard.h"
 
-unsigned char term_buffer[128] = {
-    0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-    0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-    0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-    0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-    0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-    0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-    0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-    0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, '/n'
-    };
-
+/*
+ * terminal_open
+ *   DESCRIPTION: Initialize the terminal
+ *   INPUTS: filename
+ *   OUTPUTS: 0
+ *   RETURN VALUE: term_buf_location set to 0
+ */
 int terminal_open(const int* filename){
     term_buf_location = 0;
     return 0;
 }
 
+/*
+ * terminal_close
+ *   INPUTS: fd
+ *   OUTPUTS: 0
+ *   RETURN VALUE: nothing
+ */
 int terminal_close(int fd){
-return 0;
+    return 0;
 }
 
-
+/*
+ * terminal_read
+ *   DESCRIPTION: buf is now kb_buf
+ *   INPUTS: fd, buf, nybtes
+ *   OUTPUTS: 0
+ *   RETURN VALUE: buf is now kb_buffer
+ */
 int terminal_read(int fd, unsigned char* buf, int nbytes){
-return 0;
+    buf = kb_buffer;
+    return 0;
 }
 
+/*
+ * terminal_write
+ *   DESCRIPTION: write buf to screen
+ *   INPUTS: fd, buf, nbytes
+ *   OUTPUTS: 0
+ *   RETURN VALUE: screen is now buff
+ */
 int terminal_write(int fd, unsigned char* buf, int nbytes){
     int i;
     for(i=0; i<nbytes; i++)
         putc(buf[i]);
-return 0;
+    return 0;
 }
+
