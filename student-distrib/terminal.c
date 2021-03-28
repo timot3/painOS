f0ef1706c0051f501a6ca6f609021e47cc81353b
jbabbo2 <jbabbo2@illinois.edu>
2021-03-28 21:37:52 +0000

    Update terminal.c

--- student-distrib/terminal.c
+++ student-distrib/terminal.c
@@ -34,17 +34,38 @@ int terminal_close(int fd){
 }
 
 /*
- * terminal_read
- *   DESCRIPTION: buf is now kb_buf
- *   INPUTS: fd, buf, nybtes
+ * terminal_buf_save
+ *   DESCRIPTION: term_buf is now kb_buf
+ *   INPUTS:buf
  *   OUTPUTS: 0
- *   RETURN VALUE: buf is now kb_buffer
+ *   RETURN VALUE: term_buf is now kb_buffer
  */
-int terminal_read(int fd, unsigned char* buf, int nbytes){
+int terminal_buf_save(unsigned char* buf){
     int i;
     for(i=0; i<TERM_BUF_SIZE; i++)
         buf[i] = kb_buffer[i];
-    buf = kb_buffer;
+    return 0;
+}
+
+/*
+ * terminal_read
+ *   DESCRIPTION: buf is now term_buf
+ *   INPUTS: fd, buf, nybtes
+ *   OUTPUTS: 0
+ *   RETURN VALUE: buf is now term_buf
+ */
+int terminal_read(int fd, unsigned char* buf, int nbytes){
+    term_read_flag = 0;
+
+    if(nbytes < TERM_BUF_SIZE)
+        return -1;
+        
+    while(term_read_flag == 0){
+
+    }  
+    int i;
+    for(i=0; i<TERM_BUF_SIZE; i++)
+        buf[i] = term_buf[i];
     return 0;
 }
 
