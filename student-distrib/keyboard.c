152f801e85069e9cbdfe12d73ee0460521898349
jbabbo2 <jbabbo2@illinois.edu>
2021-03-28 21:37:35 +0000

    Update keyboard.c

--- student-distrib/keyboard.c
+++ student-distrib/keyboard.c
@@ -215,8 +215,9 @@ void keyboard_print(int byte) {
 
     //get correct newline behavior
     if (c == '\n' || c == '\r'){
-        terminal_read(0, terminal_buf, 0);
+        terminal_buf_save(terminal_buf);
         term_buf_location = 0;
+        term_read_flag = 1;
         reset_buffer();
     }
     if (term_buf_location >= TERM_BUF_SIZE - 1)
