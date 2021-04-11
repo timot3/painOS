b4bdac192d07ffff4748a81965838cbcb460ded4
jbabbo2 <jbabbo2@illinois.edu>
2021-04-11 22:09:57 +0000

    syscall fixes

--- student-distrib/syscall.c
+++ student-distrib/syscall.c
@@ -10,42 +10,6 @@ int32_t halt (uint8_t status) {
     return status;
 
 }
-/* int32_t execute (const uint8_t* command) {
-    int i, ret;
-    uint8_t buf[CMD_MAX_LEN];
-    // find file by name --> use func from filesys
-
-    // parse until first space
-    for (i = 0; i < CMD_MAX_LEN; i++) {
-        if (command[i] == ' ' || command[i] == '\0') {
-            if (i == 0) return -1;
-            break;
-        }
-        buf[i] = command[i];
-    }
-
-    // make sure that the file exists, otherwise return -1
-    if (file_open(buf) == -1) return -1;
-
-    // check the first four characters for 0: 0x7f; 1: 0x45; 2: 0x4c; 3: 0x46
-    // as per the MP documentation
-    uint8_t first_32_bytes[CMD_MAX_LEN];
-    file_read(0, first_32_bytes, CMD_MAX_LEN);
-    if (first_32_bytes[0] != 0x7f || first_32_bytes[1] != 0x45 || first_32_bytes[2] != 0x4c || first_32_bytes[3] != 0x46)
-        return -1;
-
-    // TODO: later checkpoint
-    // save arguments from command --> need for getargs
-
-    // Get entry point
-
-    // Set up paging
-
-    // return 256 if dies by exception, value in range 0-255 if program executes a halt syscall,
-    // then we return specified value in halt
-
-    return ret_status;
-} */
 
 int32_t execute (const uint8_t* command) {
     int pid = assign_pid();
