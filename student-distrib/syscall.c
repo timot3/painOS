b21bcc80673bdb501ecc4400985d0741d278f191
jbabbo2 <jbabbo2@illinois.edu>
2021-04-19 03:06:03 +0000

    memset > for loop

--- student-distrib/syscall.c
+++ student-distrib/syscall.c
@@ -291,8 +291,8 @@ int parse_command(const uint8_t* command, pcb_t* pcb, int pid, dentry_t *dentry)
 
     int loc = 0;
 
-    for(i = 0; i < TERM_BUF_SIZE - CMD_MAX_LEN - 1; i++)
-        pcb -> argument_buf[i] = 0;
+    //clear buffer
+    memset(pcb -> argument_buf, 0, TERM_BUF_SIZE - CMD_MAX_LEN - 1);
 
     while(command[loc] != '\0' && command[loc] != '\n' && command[loc] != ' ') {
         exec_buf[loc] = command[loc];
