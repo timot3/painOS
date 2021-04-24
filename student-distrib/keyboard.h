966e167de6186e831b3a3028cafa57e497a456cb
jbabbo2 <jbabbo2@illinois.edu>
2021-04-24 21:02:47 +0000

    keyboard prep

--- student-distrib/keyboard.h
+++ student-distrib/keyboard.h
@@ -11,6 +11,27 @@
 #define CTRL_RELEASE 204
 #define ALT_PRESS 205
 #define ALT_RELEASE 206
+#define F1_PRESS 207
+#define F2_PRESS 208
+#define F3_PRESS 209
+#define F4_PRESS 210
+#define F5_PRESS 211
+#define F6_PRESS 212
+#define F7_PRESS 213
+#define F8_PRESS 214
+#define F9_PRESS 215
+#define F10_PRESS 216
+#define F1_RELEASE 217
+#define F2_RELEASE 218
+#define F3_RELEASE 219
+#define F4_RELEASE 220
+#define F5_RELEASE 221
+#define F6_RELEASE 222
+#define F7_RELEASE 223
+#define F8_RELEASE 224
+#define F9_RELEASE 225
+#define F10_RELEASE 226
+
 
 #define ASCII_L 108
 #define KB_SPACE 57
