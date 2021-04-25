6d53def6dffb380eed9cd9e5be06c2e1436ff4d5
Timothy Vitkin <tvitkin2@illinois.edu>
2021-04-24 19:53:18 -0500

    PIT

---
+++ student-distrib/pit.c
@@ -0,0 +1,5 @@
+#include "pit.h"
+#include "lib.h"
+
+// send interrupts at a frequency
+// signals it is time to move on to the next process
---
+++ student-distrib/pit.h
@@ -0,0 +1,4 @@
+#ifndef PIT_H
+#define PIT_H
+
+#endif // PIT_H
