30965ce32cf6672806c0a0f0b3a56a9bc548ed4b
jbabbo2 <jbabbo2@illinois.edu>
2021-05-01 19:21:20 +0000

    Update scheduling.h

--- student-distrib/scheduling.h
+++ student-distrib/scheduling.h
@@ -1,7 +1,7 @@
 #ifndef SCHEDULING_H
 #define SCHEDULING_H
 #include "lib.h"
-void switch_task(uint32_t fl,uint32_t esi,uint32_t ebx,uint32_t edx,uint32_t edi,uint32_t ecx,uint32_t eax,uint32_t ebp,uint32_t esp,uint32_t eip);
+void switch_task();
 #define PROGRAM_IMAGE_START 0x8000000 // 128 MB
 
 #endif // SCHEDULING.H
